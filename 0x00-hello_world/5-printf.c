c00bdc5b232b5dffa9132c19eb6e61a060c5834c
Muhammad Ahmad <muhdahmed36@gmail.com>
2022-06-30 13:07:58 -0700

    fixed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
-sh: 1: :wq: not found
- * main - prints the size of various types
- * Return: 0 if exited properly, non-zero otherwise
+#include <stdio.h>
+/*
+* main - prints the size of various types
+* Return: 0 if exited properly, non-zero otherwise
 */
 int main(void)
 {
